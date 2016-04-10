3155a39aec839785752b78e974694cfa8dc815e1
Brian Zhao <zhaoyubrian@gmail.com>
2016-04-10 17:26:30 +1000

    email

--- models.py
+++ models.py
@@ -4,3 +4,10 @@ from google.appengine.ext import ndb
 
 class UserPrefs(ndb.Model):
   user = ndb.UserProperty(auto_current_user_add=True)
+
+
+class InboundEmail(ndb.Model):
+  mail_from = ndb.StringProperty()
+  mail_to = ndb.StringProperty()
+  mail_subject = ndb.StringProperty()
+  mail_message = ndb.TextProperty()
